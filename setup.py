8e634409558dbbd2c4380eb0cabddfef44e96aff
Stephan Richter <stephan.richter@gmail.com>
2007-10-31 19:41:26 +0000

    vb.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.container',
-      version = '3.5.1',
+      version = '3.6.0',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Zope Container',
