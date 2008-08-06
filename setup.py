96e91babaa4f2e07a802aba3535a60dc7b5b7570
Christophe Combelles <ccomb@free.fr>
2008-08-06 21:29:56 +0000

    fixed ML address

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 setup(name='zope.app.container',
       version = '3.6.1dev',
       author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
+      author_email='zope-dev@zope.org',
       description='Zope Container',
       long_description=(
           read('README.txt')
