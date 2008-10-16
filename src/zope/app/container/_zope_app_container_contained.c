9e212c7b6f3d72d79a92087df712600328967265
Sidnei da Silva <sidnei.da.silva@gmail.com>
2008-10-16 18:15:23 +0000

    - Since we are not including the original proxy.h here, we have to define Py_ssize_t ourselves.

--- src/zope/app/container/_zope_app_container_contained.c
+++ src/zope/app/container/_zope_app_container_contained.c
@@ -36,6 +36,17 @@
 
 static PyObject *str_p_deactivate;
 
+#if PY_VERSION_HEX < 0x02050000 && !defined(PY_SSIZE_T_MIN)
+typedef int Py_ssize_t;
+#define PY_SSIZE_T_MAX INT_MAX
+#define PY_SSIZE_T_MIN INT_MIN
+typedef Py_ssize_t (*lenfunc)(PyObject *);
+typedef PyObject *(*ssizeargfunc)(PyObject *, Py_ssize_t);
+typedef PyObject *(*ssizessizeargfunc)(PyObject *, Py_ssize_t, Py_ssize_t);
+typedef int(*ssizeobjargproc)(PyObject *, Py_ssize_t, PyObject *);
+typedef int(*ssizessizeobjargproc)(PyObject *, Py_ssize_t, Py_ssize_t, PyObject *);
+#endif
+
 typedef struct {
   cPersistent_HEAD
   PyObject *po_weaklist;
