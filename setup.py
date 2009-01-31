68ea10914a01c00eacaeabca9bdd60c734cd8b0f
Martijn Faassen <faassen@startifact.com>
2009-01-31 15:48:19 +0000

    remove dev marker

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.container',
-      version = '3.7.0dev',
+      version = '3.7.0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Container',
