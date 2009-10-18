e1f64be044887e925650dfdb6d2fa4ec6d50d0f3
Hanno Schlichting <hanno@hannosch.eu>
2009-10-18 10:18:50 +0000

    Rerelease 3.9.0 as it had a broken Windows 2.6 egg.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.container',
-      version = '3.9.1dev',
+      version = '3.9.1',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Container',
