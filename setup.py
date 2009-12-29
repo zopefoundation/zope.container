3d0d3bde697f2b13f7006f2ef3d693dbfdb2d61c
Hanno Schlichting <hanno@hannosch.eu>
2009-12-29 20:30:58 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.container',
-      version = '3.10.1',
+      version = '3.10.2dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Container',
