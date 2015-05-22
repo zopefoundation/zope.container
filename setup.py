ef66f0b0f1b61394db7e3e12744f6c1f0b6725f3
Tres Seaver <tseaver@palladion.com>
2015-05-22 07:36:52 -0400

    svb

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ else:
 
 
 setup(name='zope.container',
-      version='4.1.0',
+      version='4.1.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Container',
