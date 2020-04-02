1381b11e3195b30c40b68b3c1e1a3160d0a8004a
Jason Madden <jamadden@gmail.com>
2020-04-02 05:40:24 -0500

    Back to development: 4.4.1

--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ extras['test'] += (extras['zodb'] + extras['zcml'])
 
 
 setup(name='zope.container',
-      version='4.4.0',
+      version='4.4.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Container',
