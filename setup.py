4bb562436f89cf2e561d63292893d106bee73087
Michael Howitz <mh@gocept.com>
2021-11-19 16:06:05 +0100

    Back to development: 4.5.1

--- setup.py
+++ setup.py
@@ -180,7 +180,7 @@ extras['test'] += (extras['zodb'] + extras['zcml'])
 
 
 setup(name='zope.container',
-      version='4.5.0',
+      version='4.5.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Container',
